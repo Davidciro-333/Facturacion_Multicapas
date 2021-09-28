6ac3795af1a00c4beed186c888247cb31cd75af8
Davidciro-333 <davidcirortiz06@gmail.com>
2021-09-28 13:12:11 -0500

    CapaAccesoDatos y CapaLogicaDeNegocios

---
+++ CapaAccesoDatos/clsAccesoDatos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaAccesoDatos
+{
+
+    public class clsParametros
+    {
+        public string Nombre { get; set; }
+        public object Valor { get; set; }
+        public SqlDbType TipoDato { get; set; }
+        public Int32 Tamaño { get; set; }
+        public ParameterDirection direccionParametro { get; set; }
+
+        // Constructor parametros de entrada
+        public clsParametros(string objNombre, Object objValor)
+        {
+            Nombre = objNombre;
+            Valor = objValor;
+            direccionParametro = ParameterDirection.Input;
+        }
+
+        // Constructor parametros de salida
+        public clsParametros(string objNombre, SqlDbType objTipoDato, Int32 objTamaño)
+        {
+            Nombre = objNombre;
+            TipoDato = objTipoDato;
+            Tamaño = objTamaño;
+            direccionParametro = ParameterDirection.Output;
+        }
+
+    }
+
+    public class clsAccesoDatos
+    {
+    }
+}
---
+++ CapaLogicaDeNegocios/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaLogicaDeNegocios
+{
+    public class cls_Parametros
+    {
+
+    }
+    public class Class1
+    {
+    }
+}
