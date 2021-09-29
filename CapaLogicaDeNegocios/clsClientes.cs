02e1c13fcc8e15296ca4d078417c0a70eef5e5a3
Davidciro-333 <davidcirortiz06@gmail.com>
2021-09-28 21:54:36 -0500

    clsValidarUsuario completo

--- CapaLogicaDeNegocios/clsClientes.cs
+++ CapaLogicaDeNegocios/clsClientes.cs
@@ -4,11 +4,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CapaAccesoDatos;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace CapaLogicaDeNegocios
 {
     class clsClientes
     {
+        // DEFINIMOS LO ATRIBUTOS DE LA CLASE
+        public int C_IdCliente { get; set; }
+        public string C_Nombre { get; set; }
+        public double C_Documento { get; set; }
+        public string C_Direccion { get; set; }
+        public string C_Telefono { get; set; }
+        public string C_Email { get; set; }
+        public string C_UsuarioModifica { get; set; }
 
+        clsAccesoDatos clsAcceso = new clsAccesoDatos();
+
+        public string ActualizarCliente()
+        {
+            string Mensaje = "";
+
+            try
+            {
+
+                List<clsParametros> parametros = new List<clsParametros>();
+                parametros.Add(new clsParametros("@IdCliente", C_IdCliente));
+                parametros.Add(new clsParametros("@StrNombre", C_Nombre));
+                parametros.Add(new clsParametros("@NumDocumento", C_Documento));
+                parametros.Add(new clsParametros("@StrDireccion", C_Direccion));
+                parametros.Add(new clsParametros("@StrTelefono", C_Telefono));
+                parametros.Add(new clsParametros("@StrEmail", C_Email));
+                parametros.Add(new clsParametros("@DtmFechaModifica", DateTime.Now));
+                parametros.Add(new clsParametros("@StrUsuarioModifica", C_UsuarioModifica));
+
+                Mensaje = clsAcceso.ejecutarProcedimento("actualizar_Cliente", parametros);
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "FALLÓ LA ACTUALIZACIÓN DEL CLIENTE " + ex;
+            }
+
+            return Mensaje;
+        }
+
+        public string EliminarCliente()
+        {
+            string Mensaje = "";
+
+            try
+            {
+                string Sentencia = $"Exec Eliminar_Cliente {C_IdCliente}";
+                Mensaje = clsAcceso.ejecutarComando(Sentencia);
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "FALLÓ EL BORRADO DEL CLIENTE " + ex;
+            }
+
+            return Mensaje;
+        }
+
+        public DataTable ConsultarCliente()
+        {
+            string Sentencia;
+
+            try
+            {
+                Sentencia = "SELECT * FROM TBLCLIENTES";
+                DataTable dataTable = new DataTable();
+                dataTable = clsAcceso.ejecutarConsulta(Sentencia);
+                return dataTable;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
     }
 }
--- CapaLogicaDeNegocios/clsValidarUsuario.cs
+++ CapaLogicaDeNegocios/clsValidarUsuario.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CapaAccesoDatos;
+using System.Data;
 
 namespace CapaLogicaDeNegocios
 {
     class clsValidarUsuario
     {
+        // DEFINIMOS LOS ATRIBUTOS DE LA CLASE
+        public string C_StrUsuario { get; set; }
+        public string C_StrClave { get; set; }
+        public Int32 C_IdEmpleado { get; set; }
+
+        clsAccesoDatos clsAcceso = new clsAccesoDatos();
+
+        public void ValidarUsuario()
+        {
+            try
+            {
+                string Sentencia = $"SELECT IdEmpleado, StrUsuario FROM TBLSEGURIDAD WHERE StrUsuario = {C_StrUsuario} AND StrClave = {C_StrClave}";
+                DataTable dataTable = new DataTable();
+                dataTable = clsAcceso.ejecutarConsulta(Sentencia);
+
+                foreach (DataRow fila in dataTable.Rows)
+                    C_IdEmpleado = int.Parse(fila[0].ToString());
+            }
+            catch (Exception ex)
+            {
+
+                throw(ex);
+            }
+        }
     }
 }
